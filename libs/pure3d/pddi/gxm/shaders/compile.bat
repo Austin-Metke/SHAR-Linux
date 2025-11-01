dc52c9ec0f1eb5514fc528dde3a07ad42db7cb9d
ZenoArrows <129334871+ZenoArrows@users.noreply.github.com>
2025-11-02 01:33:36 +0900

    gxm: Fix semantics and multiplaction order in vertex shader

--- libs/pure3d/pddi/gxm/shaders/compile.bat
+++ libs/pure3d/pddi/gxm/shaders/compile.bat
@@ -6,3 +6,4 @@
 "%SCE_PSP2_SDK_DIR%\host_tools\build\bin\psp2bin.exe" texture_cg.gxp -b2e PSP2,_binary_texture_gxp_start,_binary_texture_gxp_size,4 -o texture_gxp.obj
 "%SCE_PSP2_SDK_DIR%\host_tools\bin\psp2cgc.exe" --profile sce_fp_psp2 alpha.cg
 "%SCE_PSP2_SDK_DIR%\host_tools\build\bin\psp2bin.exe" alpha_cg.gxp -b2e PSP2,_binary_alpha_gxp_start,_binary_alpha_gxp_size,4 -o alpha_gxp.obj
+pause
